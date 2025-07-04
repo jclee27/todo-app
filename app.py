2e66f75b12ba411d96d4da73e027dde39df02fa0
jc lee <firstcolor@naver.com>
2025-07-04 18:13:30 +0900

    modified app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def toggle(task_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",8000))
     app.run(debug=True, host="0.0.0.0",port=port)
