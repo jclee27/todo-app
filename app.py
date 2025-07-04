9cfb2fdd5d07938f15256a29f5b6befdd63f90be
jc lee <firstcolor@naver.com>
2025-07-04 19:29:51 +0900

    fix: chage flask port number for openshift

--- app.py
+++ app.py
@@ -28,5 +28,4 @@ def toggle(task_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",8000))
-    app.run(debug=True, host="0.0.0.0",port=port)
+    app.run(debug=True, port=5000, host="0.0.0.0")
